edf422e2b668c57ebfae2ad52f2d83ee96c90ef4
Ivan Teplyakov <exreanimator@gmail.com>
2014-06-13 16:26:07 +0400

    Added deployer and log to message

--- lib/capistrano/notifier/mail.rb
+++ lib/capistrano/notifier/mail.rb
@@ -4,15 +4,20 @@ require 'action_mailer'
 
 class Capistrano::Notifier::Mailer < ActionMailer::Base
   if ActionMailer::Base.respond_to?(:mail)
-    def notice(to, from, delivery_method, stage, branch, revision)
+    def notice(to, from, delivery_method, stage, branch, revision, prev, log, deployer)
+
       text =<<-MSG
         Deployment completed.
+        Author:\t #{deployer}
         Branch:\t #{branch}
         Stage:\t #{stage}
         Rev:\t #{revision}
+
+        #{log}
+
       MSG
       mail({
-        subject: "Deployed [#{branch}] to #{stage}",
+        subject: "#{deployer} has deployed the #{stage}",
         body: text,
         delivery_method: delivery_method,
         from: from,
